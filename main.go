5b7a1f0a7bd5165d9fea59dab63f2743412efdda
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-07-02 15:22:09 +0430

    Added docker for development + debuger

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
+	http.HandleFunc("/", HelloServer)
+	fmt.Println(http.ListenAndServe(":8000", nil))
+}
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	name := "Hello world1"
+	fmt.Fprintf(w, name)
 
 }
