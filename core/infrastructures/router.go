2f7772855d135af08233e0aec1096e36d477a8df
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-07-03 09:57:15 +0430

    Added router infrastructure

---
+++ core/infrastructures/router.go
@@ -0,0 +1,25 @@
+package infrastructures
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+type Router struct {
+	Gin *gin.Engine
+}
+
+func NewRouter(env Env) Router {
+	httpRouter := gin.Default()
+
+	httpRouter.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+	}))
+
+	return Router{
+		Gin: httpRouter,
+	}
+}
