bd046eef82a50a7afdbe36e23e0c55dc436cb34d
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-07-04 14:58:36 +0430

    Added user model + tables

---
+++ core/migrations/000001_user_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "users" 
+("id" BIGSERIAL,
+"created_at" TIMESTAMPTZ,
+"updated_at" TIMESTAMPTZ,
+"deleted_at" TIMESTAMPTZ,
+"email" VARCHAR (50) UNIQUE NOT NULL,
+"password" VARCHAR (256) NOT NULL,
+"first_name" VARCHAR (60) NOT NULL,
+"last_name" VARCHAR (60) NOT NULL,
+PRIMARY KEY ("id")
+);
+CREATE INDEX "idx_users_deleted_at" ON "users" ("deleted_at");
+CREATE INDEX "idx_users_created_at" ON "users" ("created_at");
--- core/models/user.go
+++ core/models/user.go
@@ -3,8 +3,61 @@ package models
 type User struct {
 	Base
 	IsAdmin   bool   `json:"-"`
-	Email     string `json:"email" binding:"required" gorm:"unique"`
+	Email     string `json:"email" binding:"required"`
 	FirstName string `json:"firstName" binding:"required"`
 	LastName  string `json:"lastName" binding:"required"`
 	Password  string `json:"password" binding:"required"`
+}
+
+// TableName gives table name of model
+func (m User) TableName() string {
+	return "users"
+}
+
+// ToMap convert User to map
+func (m User) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"Email":     m.Email,
+		"FirstName": m.FirstName,
+		"LastName":  m.LastName,
+		"IsAdmin":   m.IsAdmin,
+	}
+}
+
+func (u User) ToResponse() UserResponse {
+	return UserResponse{
+		BaseResponse: BaseResponse{
+			CreatedAt: u.CreatedAt.Unix(),
+			UpdatedAt: u.UpdatedAt.Unix(),
+			ID:        u.ID,
+		},
+		Email:     u.Email,
+		FirstName: u.FirstName,
+		LastName:  u.LastName,
+		Password:  u.Password,
+	}
+}
+
+func UsersToResponses(users []User) []UserResponse {
+	userResponses := make([]UserResponse, len(users))
+	for i, v := range users {
+		userResponses[i] = v.ToResponse()
+	}
+	return userResponses
+}
+
+type UserResponse struct {
+	BaseResponse
+	Email     string `json:"email"`
+	FirstName string `json:"firstName" binding:"required"`
+	LastName  string `json:"lastName" binding:"required"`
+	Password  string `json:"-"`
+}
+
+type CreateUserRequestAdmin struct {
+	Email          string `json:"email" binding:"required,uniqueDB=users&email,email"`
+	FirstName      string `json:"firstName" binding:"required"`
+	LastName       string `json:"lastName" binding:"required"`
+	Password       string `json:"password" binding:"required"`
+	RepeatPassword string `json:"repeatPassword" binding:"required,eqfield=Password"`
 }
