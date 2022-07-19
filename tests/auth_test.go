89bc2ba93a8ced8982d9b84589f70846ca92d173
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-07-19 12:44:15 +0430

    Base of authentication test

--- tests/auth_test.go
+++ tests/auth_test.go
@@ -1,8 +1,21 @@
 package tests
 
-import "boilerplate/tests/mocks"
+import (
+	"boilerplate/apps/user/controllers"
+	"boilerplate/apps/user/services"
+	"boilerplate/core/infrastructures"
+	"boilerplate/core/utils"
+	"boilerplate/tests/mocks"
+)
 
 func test_register_good_data() {
-	userRepository := mocks.UserRepository{}
+	env := utils.GetEnv()
+	userRepository := mocks.NewUserRepository()
+	logger := infrastructures.NewLogger(env)
+	encryption := infrastructures.NewEncryption(logger, env)
+	db := utils.GetDB(env)
 
+	userService := services.NewUserService(userRepository, db, logger, encryption)
+	userController := controllers.NewUserController(logger, env, userService)
+	c := userController.CreateUser()
 }
